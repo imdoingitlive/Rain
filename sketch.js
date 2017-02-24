c91f6e150a83a3d43a73348f16f622550824a5b2
Edwin Rivera <edwin.rivera@outlook.com>
2017-02-23 19:27:28 -0500

    created a loop to render multiple streams on the page

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var symbolSize = 60;
-var stream;
+var streams = [];
 
 function setup() {
     createCanvas(
@@ -7,14 +7,21 @@ function setup() {
         window.innerHeight
     );
     background(0);
-    stream = new Stream();
-    stream.generateSymbols();
+    var x = 0;
+    for (var i = 0; i <= width / symbolSize; i++) {
+        var stream = new Stream();
+        stream.generateSymbols(x, random(-1000, 0));
+        streams.push(stream);
+        x += symbolSize;
+    }
     textSize(symbolSize);
 }
 
 function draw() {
-    background(0);
-    stream.render();
+    background(0, 150);
+    streams.forEach(function(stream) {
+        stream.render();
+    });
 }
 
 function Symbol(x, y, speed) {
@@ -42,9 +49,7 @@ function Stream() {
     this.totalSymbols = round(random(5, 30));
     this.speed = random(5, 20);
 
-    this.generateSymbols = function() {
-        var y = 0;
-        var x = width / 2;
+    this.generateSymbols = function(x, y) {
 
         for (var i = 0; i <= this.totalSymbols; i++) {
             symbol = new Symbol(x, y, this.speed);

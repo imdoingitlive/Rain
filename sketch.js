7bc8fd5a0b6e9411c6ec15fcfd916ef8e8961262
Edwin Rivera <edwin.rivera@outlook.com>
2017-02-23 19:03:23 -0500

    set up if statement to control the switch interval of the character

--- sketch.js
+++ sketch.js
@@ -26,17 +26,21 @@ function Symbol(x, y, speed) {
     this.y = y;
     this.value;
     this.speed = speed;
+    this.switchInterval = round(random(2, 20));
 
     this.setToRandomSymbol = function() {
-        this.value = String.fromCharCode(
-            0x30A0 + round(random(0, 96))
-        );
+        if (frameCount % this.switchInterval == 0) {
+            this.value = String.fromCharCode(
+                0x30A0 + round(random(0, 96))
+            );
+        }
     }
 
     this.render = function() {
         fill(0, 255, 70);
         text(this.value, this.x, this.y);
         this.rain();
+        this.setToRandomSymbol();
     }
 
     this.rain = function() {
